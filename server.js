f4f333aecde74ec07bd140d97250810391c21838
Priti P <pritip2427@gmail.com>
2026-03-31 22:39:01 +0530

    Fix image asset content types for Render

--- server.js
+++ server.js
@@ -180,6 +180,12 @@ const contentTypes = {
   ".css": "text/css; charset=utf-8",
   ".js": "application/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".svg": "image/svg+xml",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".webp": "image/webp",
+  ".gif": "image/gif",
 };
 
 function sendJson(response, statusCode, data) {
